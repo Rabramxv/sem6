e27a4a80ee709b9524da42f6d3f820ffd78f5502
Roman Krotov <krotan152@gmail.com>
2024-03-11 17:01:47 +0300

    add task2

---
+++ task2/Program.cs
@@ -0,0 +1,9 @@
+﻿public class Task2
+{
+    public static void Main(string[] args)
+    {
+        string input = "WhaTssUPBrO.-";
+        string result = input.ToLower();
+        Console.WriteLine(result);
+    }
+}
